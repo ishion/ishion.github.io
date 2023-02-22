bfd124bd6a7878217c2458dde29f96d9377b4133
ishion <35907991+ishion@users.noreply.github.com>
2023-02-22 17:04:31 +0800

    Add files via upload

---
+++ file/config.js
@@ -0,0 +1,5 @@
+window.hm_url = "";
+window.line_1 = "https://www.baidu.com/";
+window.line_2 = "https://www.baidu.com/";
+window.line_3 = "https://www.baidu.com/";
+window.default_line = "https://www.baidu.com/";
