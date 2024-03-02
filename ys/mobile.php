c2c24ca00a7a9de0f2e765deb1274b58d05ecf14
ishion <35907991+ishion@users.noreply.github.com>
2024-03-02 08:52:20 +0800

    Create mobile.php

---
+++ ys/mobile.php
@@ -0,0 +1,10 @@
+{
+	"AppName": "青豆影视",
+	"VersionName": "2.2.8",
+	"VersionCode": "228",
+	"MustUpdate": "1",
+	"Url": "http://www.zhoudashen.com/apk/release/apk.html",
+	"CheckType": "1",
+	"DownloadUrl": "http://down.zhoudashen.com/1653577/qingdou/qingdou2.2.8.apk"
+}
+
